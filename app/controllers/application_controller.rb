3d5e5f643c4db4f3d98571d4715e585cea9e8c5c
Chumakov123 <71515689+Chumakov123@users.noreply.github.com>
2023-01-04 13:12:41 +0300

    Fix registration 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,30 @@
 class ApplicationController < ActionController::Base
   def authenticate_user
-    @current_user = User.find_by_id(session[:user_id])
-    redirect_to login_path unless @current_user.present?
+    if User.count > 0
+      @current_user = User.find_by_id(session[:user_id])
+      redirect_to login_path unless @current_user.present?
+    else
+      redirect_to login_path
+    end
   end
 
   def non_authenticate_user
-    @current_user = User.find_by_id(session[:user_id])
-    redirect_to login_path if @current_user.present?
+    if User.count > 0
+      @current_user = User.find_by_id(session[:user_id])
+      redirect_to root_path if @current_user.present?
+    else
+      redirect_to root_path
+    end
   end
 
   def check_admin
-    @current_user = User.find_by_id(session[:user_id])
-    @admin_user = AdminUser.find_by_email(@current_user.email)
-    redirect_to root_path unless @admin_user.present?&&@admin_user.present?
+    if User.count > 0 && AdminUser.count > 0
+      @current_user = User.find_by_id(session[:user_id])
+      @admin_user = AdminUser.find_by_email(@current_user.email)
+      redirect_to root_path unless @current_user.present?&&@admin_user.present?
+    else
+      redirect_to root_path
+    end
   end
 
   protected
